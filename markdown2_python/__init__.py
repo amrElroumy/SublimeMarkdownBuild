d4df489f2fab65f2a82af8dd1a22e8fd1808702b
amrElroumy <amr.elroumy@gmail.com>
2012-06-23 23:42:46 +0200

    Added pygments as a submodule Added css rules for syntax highlighting Imported necessary modules in markdown2

---
+++ markdown2_python/__init__.py
@@ -0,0 +1,6 @@
+try:
+    import pygments.formatters.html
+    from pygments.styles import default
+    from pygments.lexers import *
+except:
+    print "Failed to import pygments' necessary modules"
